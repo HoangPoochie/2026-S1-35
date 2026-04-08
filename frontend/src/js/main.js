cb4f3f29cf2762c0e8f307b7b3f316cc89e743ca
Hoang Milan <ndnhoang@gmail.com>
2026-04-08 18:28:55 +0930

    Task: Implement the npm to the front end.
    Details: The frontend can now run with command npm run dev. Files touched: main.js, index.html (to include script from main.js), and vite.config.js
    
    Next: Follows Vite-native approach. Not many separate .html pages, but one main HTML entry and many Js modules/components representing pages.

---
+++ frontend/src/js/main.js
@@ -0,0 +1,12 @@
+import "../styles/styles.css";
+
+document.querySelector("#app").innerHTML = `
+  <nav class="navbar">
+    <a href="/">Home</a>
+    <a href="/src/pages/topics/self-management-skills/self-awareness.html">Self Awareness</a>
+  </nav>
+
+  <section class="hero">
+    <h1>Best Version of Me</h1>
+  </section>
+`;
