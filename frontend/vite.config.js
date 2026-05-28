fd14d8108cc9c715dd4deb5a3b51554e31b4d993
ItsNesh <52585845+ItsNesh@users.noreply.github.com>
2026-05-28 10:19:26 +0930

    allows app to be accessed from the internet via Cloudflare tunnel

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from "vite";
 export default defineConfig({
   server: {
     host: "0.0.0.0",
+    allowedHosts: ["bvom.nesh.gg"],
     proxy: {
       "/api": {
         target: process.env.VITE_PROXY_TARGET || "http://localhost:8080",
